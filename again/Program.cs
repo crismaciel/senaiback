9c127fcd9978632c3e86e2f156dff2d097d3b6d5
FIC <bruno_afonso95@hotmail.com>
2017-10-06 20:41:24 -0300

    again

--- again/Program.cs
+++ again/Program.cs
@@ -5,8 +5,49 @@ namespace ConsoleCadastro
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Olá Mundo!");
-        }
+            {
+            Console.WriteLine("Nome completo:");
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine("Idade:");
+            string idade = Console.ReadLine();
+            
+            Console.WriteLine("Genero:");
+            string genero = Console.ReadLine();
+            
+            Console.WriteLine("Estado civil:");
+            string estado = Console.ReadLine();
+            
+            Console.WriteLine("Local nascimento:");
+            string local = Console.ReadLine();
+
+            Console.WriteLine("RG:");
+            string rg = Console.ReadLine();
+
+            Console.WriteLine("CPF:");
+            string cpf = Console.ReadLine();
+
+            Console.WriteLine("Endereco:");
+            string endereco = Console.ReadLine();
+
+            Console.WriteLine("Profissao:");
+            string profissao = Console.ReadLine();
+
+            string mostrar = "\nNome: "+nome+"\nIdade: "+idade+"\nGenero: "+genero+"\nEestado: "+estado+ "\nLocal: "+ local + "\nRG: "+rg + "\nCPF: "+cpf +"\nEndereco: "+endereco + "\nProfissao: "+profissao;        
+            Console.WriteLine(mostrar);
+
+
+
+
+
+
+
+
+
+
+
+
+   
+            }
     }
 }
